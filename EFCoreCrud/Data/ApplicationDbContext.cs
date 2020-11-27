51a4968faf81537dc67f641f2351911d5aa7f04c
basharovi <hello@basharovi.tech>
2020-11-27 16:45:56 +0600

    Added ApplicationDbContext Configuration

---
+++ EFCoreCrud/Data/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using EFCoreCrud.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace EFCoreCrud.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        private readonly string _connectionString;
+        public ApplicationDbContext(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(_connectionString);
+            }
+
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}

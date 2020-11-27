ed71e42def5cc24c4908c6d81578a6e3c1f36c03
basharovi <hello@basharovi.tech>
2020-11-27 15:53:55 +0600

    Add project files.

---
+++ EFCoreCrud/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EFCoreCrud.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

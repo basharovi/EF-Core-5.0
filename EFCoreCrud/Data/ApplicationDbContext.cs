a4817481de486806f9ccc7f08db9642ed316d2c5
basharovi <hello@basharovi.tech>
2020-11-27 17:15:54 +0600

    Modified DbContext Configuration

--- EFCoreCrud/Data/ApplicationDbContext.cs
+++ EFCoreCrud/Data/ApplicationDbContext.cs
@@ -6,24 +6,13 @@ namespace EFCoreCrud.Data
 {
     public class ApplicationDbContext : DbContext
     {
-        private readonly string _connectionString;
-        public ApplicationDbContext(IConfiguration configuration)
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
         {
-            _connectionString = configuration.GetConnectionString("DefaultConnection");
         }
 
         public DbSet<Student> Students { get; set; }
         public DbSet<Course> Courses { get; set; }
         public DbSet<Enrollment> Enrollments { get; set; }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-                optionsBuilder.UseSqlServer(_connectionString);
-            }
-
-            base.OnConfiguring(optionsBuilder);
-        }
     }
 }
--- EFCoreCrud/Startup.cs
+++ EFCoreCrud/Startup.cs
@@ -8,6 +8,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EFCoreCrud.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace EFCoreCrud
 {
@@ -24,6 +26,9 @@ namespace EFCoreCrud
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

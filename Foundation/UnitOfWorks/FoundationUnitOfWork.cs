a6bd515cd142f29cfaf77743c94be9e187168f1f
basharovi <hello@basharovi.tech>
2020-12-01 00:27:28 +0600

    Initially Added unitOfWorks

---
+++ Foundation/UnitOfWorks/FoundationUnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using Foundation.Repositories;
+using DataAccessLayer;
+using Foundation.Context;
+
+namespace Foundation.UnitOfworks
+{
+    public class FoundationUnitOfWork : UnitOfWork, IFoundationUnitOfWork
+    {
+        public IStudentRepository StudentRepository { get; set; }
+
+        public FoundationUnitOfWork(ApplicationDbContext context,
+            StudentRepository studentRepository)
+            : base(context)
+        {
+            StudentRepository = studentRepository;
+        }
+    }
+}
---
+++ Foundation/UnitOfWorks/IFoundationUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using DataAccessLayer;
+using Foundation.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Foundation.UnitOfworks
+{
+    public interface IFoundationUnitOfWork : IUnitOfWork
+    {
+        IStudentRepository StudentRepository { get; set; }
+    }
+}
